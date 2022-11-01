d4154dbf1d927e0d9409526f6d72ca66be2bc2a8
MarcosRubi <danielhernandez9980@gmail.com>
2022-11-01 11:38:13 -0600

    Funcionamiento cambiar tema e imagen

--- src/context/TaskContext.jsx
+++ src/context/TaskContext.jsx
@@ -1,14 +1,21 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const TaskContext = createContext();
 
 export function TaskContextProvider(props) {
-    const [theme, setTheme] = useState("light");
+    const [theme, setTheme] = useState(document.querySelector("html").dataset.theme);
+
+    useEffect(() => {
+        if (theme === "dark") {
+            document.querySelector("html").setAttribute("data-theme", "dark");
+            return;
+        }
+        document.querySelector("html").setAttribute("data-theme", "light");
+    }, [theme]);
 
     return (
-            <TaskContext.Provider value={{theme, setTheme}}>
-                {props.children}
-            </TaskContext.Provider>
+        <TaskContext.Provider value={{ theme, setTheme }}>
+            {props.children}
+        </TaskContext.Provider>
     );
 }
-
